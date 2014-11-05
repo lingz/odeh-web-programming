4b700907f83e95fe3ac3543c44685052589e80b0
Lingliang Zhang <lingliangz@gmail.com>
2014-11-05 09:12:30 -0500

    improved branding

--- content-page.php
+++ content-page.php
@@ -9,10 +9,10 @@
 <div id="main">
   <article id="post-<?php the_ID(); ?>" <?php post_class(); ?>>
     <header class="header entry-header">
-        <h2>
+        <h4>
           New York University <br/>
           <?php bloginfo( 'name' ); ?>
-        </h2>
+        </h4>
         <h1>
           <?php the_title( '<h1 class="entry-title">', '</h1>' ); ?>
         </h1>
